37ef0f8dd557a6f3363764fe39805395046cc2de
andreoddone <98482734+andreoddone@users.noreply.github.com>
2025-03-15 20:24:07 -0300

    Create ProdutoController.js

---
+++ controllers/ProdutoController.js
@@ -0,0 +1,19 @@
+const Product = require('../models/Produto');
+
+exports.createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao cadastrar tinta" });
+  }
+};
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar produtos" });
+  }
+};
